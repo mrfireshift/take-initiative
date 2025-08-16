4169e3ba4824273d8b1c4ff2acd6e2786d3d0b45
mrfireshift <federicocosimo.biancullo@gmail.com>
2025-08-16 23:13:28 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import path from "node:path";
 
 export default defineConfig({
-  base: "/", // se un giorno userai GitHub Pages in sottocartella, qui cambierai
-
-  server: { cors: { origin: "https://www.owlbear.rodeo" } },
-
-  build: {
-    rollupOptions: {
-      input: {
-        main:    path.resolve(process.cwd(), "index.html"),
-        ctxAdd:  path.resolve(process.cwd(), "ctx-add.html"),
-        ctxMark: path.resolve(process.cwd(), "ctx-mark.html"),
-      },
+  server: {
+    cors: {
+      origin: "https://www.owlbear.rodeo",
     },
   },
 });
