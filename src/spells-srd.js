034d49fb3981785c366f5fe30eeb3dd8c266ef71
mrfireshift <federicocosimo.biancullo@gmail.com>
2025-08-29 16:37:04 +0200

    Take Initiative! v1.1.4

--- src/spells-srd.js
+++ src/spells-srd.js
@@ -2,6 +2,6 @@
 export const SPELLS_5E_SRD = [
   "Velocità", "Lentezza", "Scudo", "Benedizione", "Armatura Magica", "Intralciare", "Silenzio",
   "Volare", "Invisibilità", "Pelle di Pietra", "Metamorfosi", "Scudo della Fede", "Marchio del Cacciatore", 
-  "Protezione dal Bene e dal Male", "Manto del Crociato",
-  "Aura di Vitalità", "Animare Oggetti", "Trama Ipnotica"
+  "Protezione dal Bene e dal Male", "Manto del Crociato", "Spiriti Guardiani", "Luminescenza",
+  "Aura di Vitalità", "Animare Oggetti", "Trama Ipnotica", "Scudiscio Mentale di Tasha"
 ];
